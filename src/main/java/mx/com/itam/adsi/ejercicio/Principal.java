7d98b54e73b2052915a10f297ff605e30ab25cd6
VicCG <adsi.itam.26@gmail.com>
2018-03-26 10:56:13 -0600

    Primer commit de clase

---
+++ src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
@@ -0,0 +1,13 @@
+package mx.com.itam.adsi.ejercicio;
+
+class Principal {
+  public static void main(String[] args) {
+    System.out.println(toArabic("fff1","0123456789abcdef"));
+  }
+  private static int toArabic(String source, String alphabet){
+    int res=0;
+    for (char c : source.toCharArray())
+      res=res*(alphabet.length())+alphabet.indexOf(c);
+    return res;
+  }
+}
